8ef468bccffe7aff632d2faf27e73545a1813505
ngt-kn <ngt.kn.dev@gmail.com>
2018-07-17 22:47:14 -0700

    add logout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import firebase from 'firebase';
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null}
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyA4Clg59uFlaU6WAf34wyFJ7Sp3Uokvt2s',
@@ -15,13 +17,48 @@ class App extends Component {
       storageBucket: 'authreactnative-ddf90.appspot.com',
       messagingSenderId: '501791093216'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return  (
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>
+                Log Out
+              </Button>
+            </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <CardSection
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <Spinner size="large" />
+          </CardSection>
+        );
+    }
+
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+          {this.renderContent()}
       </View>
     );
   }
