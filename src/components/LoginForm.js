44f9d9933a73dda23f54976338afe4cfca00c786
ngt-kn <ngt.kn.dev@gmail.com>
2018-07-17 19:31:39 -0700

    add firebase login/sign up

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native'
 import { Button, Card, CardSection, Input } from './common';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(() => {
+        this.setState({ error: 'Authentication Failed' })
+      });
+    });
+  }
 
   render() {
     return (
@@ -26,13 +39,25 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log In
           </Button>
         </CardSection>
       </Card>
     );
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 }
 
